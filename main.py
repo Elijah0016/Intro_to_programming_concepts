8480c43d7df7b58ca1086a327a5890b461b56969
Elijah0016 <elouisjean16@gmail.com>
2026-01-20 18:08:43 -0500

    create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+print("Elijah is chill")
+#Using the pound/hastag symbol is a qway to do comments.
+print("Hello, how are you today ?")
+
+name = "Scooby" #variables unlike other languages don't have a type.
+
+#int, double, float, char, string, boolean,
+age = 21
+temperature = 98.7
+
+#types  of printing
+print("I am {} years old, and the temperature is {} degrees".format(age, temperature ))
+
+#input
+friendname = input("What is your name?\n ")
+print("The new friend name is ", friendname)
+
+fage = int(input("What is your friend age?\n "))
+print(fage)
+
+total = 52 + 78
+print(total)
+
+agestotal = fage + age
+print(agestotal)
+
+modulestotal = 5 % 2
+print(modulestotal)
